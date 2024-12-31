86aaca60cd3370bce00f4473ce391fcf38a8dc21
fumaxin <kalpeshdhoundiyal@gmail.com>
2024-12-31 08:32:55 +0530

    cors all

--- core/settings.py
+++ core/settings.py
@@ -189,10 +189,11 @@ SIMPLE_JWT = {
 }
 
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
-    "http://127.0.0.1:8000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:9000",
+#     "http://127.0.0.1:8000",
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
